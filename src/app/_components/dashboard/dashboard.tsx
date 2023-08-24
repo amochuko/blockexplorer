bb247a2125b54dfa88484ea6207ab980087b06cc
Ogheneochuko <the.undefined.null@gmail.com>
2023-08-24 11:57:36 +0100

    feat: add dashboard

---
+++ src/app/_components/dashboard/dashboard.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { getMarketCap } from '@/app/api/data';
+import { formatNumToCurrency } from '@/utils/lib';
+import { LatestBlocks } from '../block';
+import { ErrorBoundary } from '../hoc/error-boundary';
+import { useBlock } from '../hooks/useBlock';
+import { useEtherPrice } from '../hooks/useEtherPrice';
+import { BlockTransactions } from '../transaction/transactions';
+import './dashboard.scss';
+
+export function Dashboard(props: any) {
+  const { block, error, pendingTxns, finalizedBlock } = useBlock();
+  const { ethPrice } = useEtherPrice();
+
+  const data = [
+    { title: 'ether price', value: ethPrice },
+    {
+      title: 'Market cap',
+      value: `$ ${formatNumToCurrency({ amt: getMarketCap() })}`,
+    },
+    { title: 'last finalized block', value: finalizedBlock },
+    { title: 'Pending Tnx(s)', value: pendingTxns },
+  ];
+
+  return (
+    <div className='dashboard'>
+      <ErrorBoundary>
+        {error.message ? <p>error.message</p> : null}
+        <ul className='dashboard-header'>
+          {data.length > 0 &&
+            data.map((itm, i) => (
+              <li key={i} className='dashboard-list-itm'>
+                <span className='title'>{itm.title.toUpperCase()}:</span>
+                <span className='value'>{itm.value}</span>
+              </li>
+            ))}
+        </ul>
+      </ErrorBoundary>
+
+      <div className='dashboard-main'>
+        <div className='latest-block'>
+          <LatestBlocks />
+        </div>
+
+        <div className='latest-txn'>
+          <BlockTransactions block={block} />
+        </div>
+      </div>
+    </div>
+  );
+}
