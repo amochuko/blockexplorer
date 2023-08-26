dcd0fbb29b0569aef95d0a9514d3d166b8e66e94
Ogheneochuko <the.undefined.null@gmail.com>
2023-08-26 15:54:02 +0100

    fix: updated block comp

--- src/app/_components/block/details/block-detials.tsx
+++ src/app/_components/block/details/block-detials.tsx
@@ -1,82 +1,70 @@
 'use client';
-import { ethProvider } from '@/utils/provider';
 import { Block } from '@ethersproject/providers';
 import Link from 'next/link';
-import { useCallback, useEffect, useState } from 'react';
+import { Suspense } from 'react';
 import { ErrorBoundary } from '../../hoc/error-boundary';
 import './block-details.css';
 
-export function BlockDetails() {
-  const [blockLatest, setBlockLatest] = useState<Block>();
-  const [blocks, setBlocks] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const provider = ethProvider();
-
-  const getLatestBlock = useCallback(async () => {
-    const res = await provider.getBlock('latest');
-    setBlockLatest(res);
-    console.log(BlockDetails.name)
-  }, [provider]);
-
-  useEffect(() => {
-    //  getLatestBlock();
-  },[getLatestBlock, provider]);
-
+interface BlockDetailsProps {
+  block: Block;
+}
+export function BlockDetails(props: BlockDetailsProps) {
   return (
     <ErrorBoundary>
-      <div className='block'>
-        <h3>Latest Block Details</h3>
-        <div className='table'>
-          <table>
-            <tbody>
-              {blockLatest &&
-                Object.keys(blockLatest).map((key, i) => {
-                  let values = Object.values(blockLatest);
-                  return (
-                    <tr key={key + i}>
-                      <td>
-                        <span className='key'>
-                          {' '}
-                          {key.replace('_', ' ').slice(0, 1).toUpperCase() +
-                            key.slice(1)}
-                          :{' '}
-                        </span>
-                        <span className='value'>
-                          {Array.isArray(values[i]) ? (
-                            <Link href={`txs/${blockLatest['number']}`}>
-                              {Array(values[i])[0].length + ' transactions'}
-                            </Link>
-                          ) : typeof values[i] !== 'object' ? (
-                            key === 'number' ? (
-                              <Link href={`block/${values[i]}`}>
-                                {values[i]}
+      <Suspense fallback={<p>Loading block details</p>}>
+        <div className='block'>
+          <h3>Latest Block Details</h3>
+          <div className='table'>
+            <table>
+              <tbody>
+                {props.block &&
+                  Object.keys(props.block).map((key, i) => {
+                    let values = Object.values(props.block);
+                    return (
+                      <tr key={key + i}>
+                        <td>
+                          <span className='key'>
+                            {' '}
+                            {key.replace('_', ' ').slice(0, 1).toUpperCase() +
+                              key.slice(1)}
+                            :{' '}
+                          </span>
+                          <span className='value'>
+                            {Array.isArray(values[i]) ? (
+                              <Link href={`txs/${props.block['number']}`}>
+                                {Array(values[i])[0].length + ' transactions'}
                               </Link>
+                            ) : typeof values[i] !== 'object' ? (
+                              key === 'number' ? (
+                                <Link href={`block/${values[i]}`}>
+                                  {values[i]}
+                                </Link>
+                              ) : (
+                                values[i]
+                              )
+                            ) : key === 'gasUsed' ? (
+                              new Intl.NumberFormat('en', {
+                                maximumFractionDigits: 3,
+                              }).format(Number(values[i])) +
+                              ` (${(
+                                (Number(values[i]) / 30000000) *
+                                100
+                              ).toFixed(2)} %)`
                             ) : (
-                              values[i]
-                            )
-                          ) : key === 'gasUsed' ? (
-                            new Intl.NumberFormat('en', {
-                              maximumFractionDigits: 3,
-                            }).format(Number(values[i])) +
-                            ` (${((Number(values[i]) / 30000000) * 100).toFixed(
-                              2
-                            )} %)`
-                          ) : (
-                            new Intl.NumberFormat('en', {
-                              maximumFractionDigits: 3,
-                            }).format(Number(values[i]))
-                          )}
-                        </span>
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
+                              new Intl.NumberFormat('en', {
+                                maximumFractionDigits: 3,
+                              }).format(Number(values[i]))
+                            )}
+                          </span>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      </Suspense>
     </ErrorBoundary>
   );
 }
--- src/app/_components/hooks/useBlock.tsx
+++ src/app/_components/hooks/useBlock.tsx
@@ -1,18 +1,18 @@
 'use client';
+import { timeAgo } from '@/utils/lib';
 import { ethProvider } from '@/utils/provider';
 import { TransactionResponse } from '@ethersproject/abstract-provider';
 import { Block } from '@ethersproject/providers';
 import { useCallback, useEffect, useState } from 'react';
-
-interface BlockWithTransactionsProps {
-  block: Block | undefined;
-}
 
 export function useBlock(blkNumber?: number) {
   const [block, setBlock] = useState<Block>();
   const [error, setError] = useState<any>({});
   const [pendingTxns, setPendingTxns] = useState(0);
   const [finalizedBlock, setFinalizedBlock] = useState(0);
+  const [timeStamp, setTimeStamp] = useState(0);
+  const [blockLatest, setBlockLatest] = useState<Block>();
+  const [blocks, setBlocks] = useState<Block[]>([]);
   const [blockTransactions, setBlockTransactions] = useState<
     TransactionResponse[]
   >([]);
@@ -39,20 +39,59 @@ export function useBlock(blkNumber?: number) {
 
   const pendingTx = useCallback(async () => {
     const pendingBlk = await provider.getBlock('pending');
-    console.log('pendingTxn: ', pendingBlk.transactions.length);
     setPendingTxns(pendingBlk.transactions.length);
   }, []);
 
   const finalizedBlk = useCallback(async () => {
-    const fBlk = await provider.getBlock('final');
-    console.log('pendingTxn: ', fBlk);
+    const fBlk = await provider.getBlock('finalized');
     setFinalizedBlock(fBlk.number);
   }, []);
+
+  const getLatestBlock = useCallback(async () => {
+    const res = await provider.getBlock('latest');
+    setBlockLatest(res);
+  }, [provider]);
+
+  const setBlockEvent = useCallback(async (num: number) => {
+    try {
+      const blk = await provider.getBlock(num);
+
+      setBlocks((prevBlk) => [...prevBlk!, blk]);
+    } catch (err: any) {
+      setError(err.message);
+    }
+  }, []);
+
+  const getBlockTime = async (blockHash: any) => {
+    const blk = await provider.getBlock(blockHash);
+    const t = timeAgo(blk.timestamp);
+    setTimeStamp(blk.timestamp);
+  };
+
+  // subscribe to block event
+  const subscribeToBlocks = useCallback(() => {
+    provider.on('block', setBlockEvent);
+  }, [setBlockEvent]);
+
+  // unsubscribe to block event
+  const unsubscribeToBlocks = useCallback(() => {
+    provider.off('block', setBlockEvent);
+  }, [setBlockEvent]);
+
+  useEffect(() => {
+    subscribeToBlocks();
+
+    return () => {
+      // clean up
+      unsubscribeToBlocks();
+    };
+  }, [blocks, setBlockEvent, subscribeToBlocks, unsubscribeToBlocks]);
 
   useEffect(() => {
     getBlock();
     pendingTx();
     finalizedBlk();
+    getLatestBlock();
   }, []);
 
   useEffect(() => {
@@ -66,11 +105,15 @@ export function useBlock(blkNumber?: number) {
     }
   }, [block?.transactions]);
 
+  
   return {
     block,
+    blocks,
     blockTransactions,
     error,
     pendingTxns,
     finalizedBlock,
+    blockLatest,
+    timeStamp,
   };
 }
